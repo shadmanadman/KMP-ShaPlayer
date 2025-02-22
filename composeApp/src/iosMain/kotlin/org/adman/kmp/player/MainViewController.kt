e16183ec368f54b0cf20720ade1b5541e0eaebc5
shadmanadman <adman.shadman@gmail.com>
2025-02-22 22:49:44 +0330

    Implement the ios section

--- composeApp/src/iosMain/kotlin/org/adman/kmp/player/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/adman/kmp/player/MainViewController.kt
@@ -2,4 +2,4 @@ package org.adman.kmp.player
 
 import androidx.compose.ui.window.ComposeUIViewController
 
-fun MainViewController() = ComposeUIViewController {KmpShaPlayer(urlOrPathToFile = "") }
+fun MainViewController() = ComposeUIViewController {KmpShaPlayer(urlOrPathToFile = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4") }
