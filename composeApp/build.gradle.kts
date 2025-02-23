362c75a41d0b95ceb19fab5ed8dcee402de772d4
shadmanadman <adman.shadman@gmail.com>
2025-02-23 21:59:09 +0330

    Enable the use of the JavaFX library and introduce OS detection specific OpenJFX dependencies.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+    id("com.google.osdetector") version "1.7.3"
 }
 
 kotlin {
@@ -18,7 +19,7 @@ kotlin {
             jvmTarget.set(JvmTarget.JVM_11)
         }
     }
-    
+
     listOf(
         iosX64(),
         iosArm64(),
@@ -29,9 +30,9 @@ kotlin {
             isStatic = true
         }
     }
-    
+
     jvm("desktop")
-    
+
     @OptIn(ExperimentalWasmDsl::class)
     wasmJs {
         moduleName = "composeApp"
@@ -51,10 +52,10 @@ kotlin {
         }
         binaries.executable()
     }
-    
+
     sourceSets {
         val desktopMain by getting
-        
+
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
@@ -70,9 +71,25 @@ kotlin {
             implementation(libs.androidx.lifecycle.runtime.compose)
         }
         desktopMain.dependencies {
+            implementation(compose.desktop.common)
             implementation(compose.desktop.currentOs)
+            val fxSuffix = when (osdetector.classifier) {
+                "linux-x86_64" -> "linux"
+                "linux-aarch_64" -> "linux-aarch64"
+                "windows-x86_64" -> "win"
+                "osx-x86_64" -> "mac"
+                "osx-aarch_64" -> "mac-aarch64"
+                else -> throw IllegalStateException("Unknown OS: ${osdetector.classifier}")
+            }
+            implementation("org.openjfx:javafx-base:19:${fxSuffix}")
+            implementation("org.openjfx:javafx-graphics:19:${fxSuffix}")
+            implementation("org.openjfx:javafx-controls:19:${fxSuffix}")
+            implementation("org.openjfx:javafx-swing:19:${fxSuffix}")
+            implementation("org.openjfx:javafx-web:19:${fxSuffix}")
+            implementation("org.openjfx:javafx-media:19:${fxSuffix}")
             implementation(libs.kotlinx.coroutines.swing)
         }
+
     }
 }
 
