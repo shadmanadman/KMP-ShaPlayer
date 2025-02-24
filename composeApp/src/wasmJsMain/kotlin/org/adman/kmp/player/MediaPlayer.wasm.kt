1829d70e5c8a67783aa8a0e96f159cfa1e5db076
shadmanadman <adman.shadman@gmail.com>
2025-02-24 14:08:12 +0330

    Implement the media player for Wasm

--- composeApp/src/wasmJsMain/kotlin/org/adman/kmp/player/MediaPlayer.wasm.kt
+++ composeApp/src/wasmJsMain/kotlin/org/adman/kmp/player/MediaPlayer.wasm.kt
@@ -1,7 +1,17 @@
 package org.adman.kmp.player
 
+import HtmlView
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import org.w3c.dom.HTMLVideoElement
+import org.w3c.dom.events.Event
 
 @Composable
 internal actual fun MediaPlayer(
@@ -9,8 +19,46 @@ internal actual fun MediaPlayer(
     urlOrUri: String,
     isLandscape: Boolean,
     stop: Boolean,
-    onLoading: (isLoading:Boolean) -> Unit,
-    onError: (error:Throwable) -> Unit
+    onLoading: (isLoading: Boolean) -> Unit,
+    onError: (error: Throwable) -> Unit
 ) {
+    Column(
+        modifier = modifier ?: Modifier.fillMaxSize().height(300.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        HtmlView(
+            modifier = Modifier.fillMaxWidth().height(if (isLandscape) 540.dp else 300.dp),
+            factory = {
+                val video = createElement("video") as HTMLVideoElement
 
+                video.apply {
+                    setAttribute("width", "100%")
+                    setAttribute("height", "100%")
+                    setAttribute("src", urlOrUri)
+                    setAttribute("controls", "true")
+                    setAttribute("autoplay", "true")
+                    setAttribute("crossorigin", "anonymous")
+                    setAttribute("paused", stop.toString())
+
+                    addEventListener("loadeddata", {
+                        onLoading(false)
+                        println("Video loaded successfully")
+                    })
+                    addEventListener("loadstart", {
+                        onLoading(true)
+                        println("Loading video...")
+                    })
+                    addEventListener("error", { event: Event ->
+                        onLoading(false)
+                        onError(Throwable("Failed to load video: ${event.toJsReference()}"))
+                    })
+                    if (stop)
+                        pause()
+                }
+                video
+            }
+        )
+    }
 }
+
